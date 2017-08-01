9e3a5e1a356a9984c80a3c825d3a7bdbb01ddfea
Andrew Quan <quandrew@users.noreply.github.com>
2017-08-01 12:19:38 -0700

    Add function to load sections from external HTML files

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
 function navigateSection() {
+    $(window).on("load", function () {
+        loadSection(window.location.hash);
+    });
     $(".nav-link").on("click", function () {
         $(".nav-link").removeClass("active");
         $(this).addClass("active");
     });
+    $(window).on("hashchange", function () {
+        loadSection(window.location.hash);
+    });
+}
+
+function loadSection(section) {
+    $(".nav-link").removeClass("active");
+    var sections = ["about", "career", "links", "projects", "skills"];
+    var sectionWithoutHash = section.replace("#", "");
+    if ($.inArray(sectionWithoutHash, sections) > -1) {
+        $(section).addClass("active");
+        $(".section").load(`/sections/${sectionWithoutHash}.html`);
+    } else if (sectionWithoutHash == "") {
+        $("#about").addClass("active");
+        $(".section").load(`/sections/about.html`);
+    } else {
+        window.location.replace("404.html");
+    }
 }
